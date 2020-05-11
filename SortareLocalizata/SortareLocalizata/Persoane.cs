1ff951b850fc79b1ecfc3d826f55e03e365987f3
hodisanluissa <56643310+hodisanluissa@users.noreply.github.com>
2020-05-11 12:18:27 +0300

    Add files via upload

---
+++ SortareLocalizata/SortareLocalizata/Persoane.cs
@@ -0,0 +1,17 @@
+﻿namespace SortareLocalizata
+{
+    public class Persoane
+    {
+        public string nume { get; set; }
+        public string prenume { get; set; }
+        public string nationalitate { get; set; }
+        public Persoane(string nume, string prenume, string nationalitate)
+        {
+            this.nume = nume;
+            this.prenume = prenume;
+            this.nationalitate = nationalitate;
+
+        }
+
+    }
+}
